720425757b5a98001d04ff841eeca4256907aa53
arp1511 <arpitaahuja999@gmail.com>
2025-09-19 11:30:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// const express = require('express');
+// const app = express();
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Content-Security-Policy',
+//     "default-src 'self' 'unsafe-inline'; connect-src 'self' http://127.0.0.1:5500"
+//   );
+//   next();
+// });
+
+// app.listen(5500, () => {
+//   console.log('Server running on port 5500');
+// });
+// const validCredentials = {
+//     'admin': 'admin123',
+//     'user1': 'password123', 
+//     'demo': 'demo'
+// };
